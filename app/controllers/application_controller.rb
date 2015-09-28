e4419994243359eca00d4b1d928825518c4c84d9
Brad Pauly <hi@bradpauly.com>
2015-09-27 20:39:16 -0700

    Add sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.where(id: session[:user_id]).first
+  end
+
+  def start_session(user)
+    session[:user_id] = user.id
+  end
+
+  def destroy_session
+    reset_session
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    @login_form = LoginForm.new
+    @login_form = LoginForm.new(remember_me: true)
   end
 
   def create
     @login_form = LoginForm.new(params[:login_form])
+
     if user = @login_form.authenticate
-      # Session.create_for_user(user)
-      # start_session_for_user(user)
+      start_session(user)
       redirect_to registrations_path
     else
       flash.now[:notice] = "Sorry, that email & password combination didn't work."
       render :new
     end
+  end
+
+  def destroy
+    destroy_session
+    redirect_to login_path
   end
 end
